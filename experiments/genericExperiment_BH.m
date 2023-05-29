b6d6cc61e76703495347e3b46b02b7fb5878dcc2
hdavis91 <hdavis@caltech.edu>
2023-05-28 20:14:26 -0400

    Revert "change experiment scheme"
    This reverts commit 5dc5333bae0df91921090111141c174ae4235f6f.

--- experiments/genericExperiment_BH.m
+++ experiments/genericExperiment_BH.m
@@ -37,14 +37,13 @@ function vr = runtimeCodeFun(vr)
 endPosition = 110;
 reward_pos=endPosition*0.8;
 zap_pos=endPosition*0.3;
-stim_lap=[1:1000];
+stim_lap=[10 11];
 
 if (vr.rig.shouldResetPosition)
     vr.position(1:4) = vr.worlds{vr.currentWorld}.startLocation;
     vr.rig.shouldResetPosition = false;
     vr.position(2)=endPosition+1;
 end
-    
 
 % trackLength = eval(vr.exper.variables.fullLength);
 
@@ -52,7 +51,7 @@ if vr.position(2) > endPosition % test if the animal is at the end of the track
     %vr.rig.reward();
     vr.position(2) = -15; % set the animal2s y position to 0
     vr.dp(:) = 0; % prevent any additional movement during teleportation
-    if (vr.rig.isAcquiring)
+    if true %(vr.rig.isRecording)
         vr.trialNumber = vr.trialNumber + 1;
         vr.r_av(vr.trialNumber)=1;
         vr.s_av(vr.trialNumber)=1;
@@ -72,12 +71,12 @@ if vr.textClicked == 1 % check if textbox #1 has been clicked
 end
 
 if vr.trialNumber>0
-    if vr.position(2)>vr.reward_pos(vr.trialNumber) && vr.r_av(vr.trialNumber)==1
-        vr.rig.reward();
-        vr.r_av(vr.trialNumber)=0;
-     %   vr.reward_pos(vr.trialNumber+1)=0;
-        vr.trialNumber
-    end
+if vr.position(2)>vr.reward_pos(vr.trialNumber) && vr.r_av(vr.trialNumber)==1
+    vr.rig.reward();
+    vr.r_av(vr.trialNumber)=0;
+ %   vr.reward_pos(vr.trialNumber+1)=0;
+    vr.trialNumber
+end
 end
 
 if ismember(vr.trialNumber,stim_lap)
@@ -88,19 +87,23 @@ if ismember(vr.trialNumber,stim_lap)
     end
 end
 
-timestamp = now;
+if (vr.rig.isRecording)
+    timestamp = now;
 
-% write timestamp and the x & y components of position and velocity to a file
-% using floating-point precision
-fwrite(vr.fid, [timestamp vr.rig.latestEncoderReading vr.position vr.trialNumber vr.rig.isAcquiring],'double');
+    % write timestamp and the x & y components of position and velocity to a file
+    % using floating-point precision
+    fwrite(vr.fid, [timestamp vr.rig.latestEncoderReading vr.position vr.trialNumber],'double');
+
+end
 
 
 
 
 % --- TERMINATION code: executes after the ViRMEn engine stops.
 function vr = terminationCodeFun(vr)
-%if (vr.rig.isRecording)
-%    filepath = fileparts(which('virmenLog.data')) + "\virmenLog.data"
-%    writeline(vr.rig.server, filepath)
-%    fclose(vr.fid);
-%end
+if (vr.rig.isRecording)
+    filepath = fileparts(which('virmenLog.data')) + "\virmenLog.data"
+    writeline(vr.rig.server, filepath)
+    fclose(vr.fid);
+
+end
